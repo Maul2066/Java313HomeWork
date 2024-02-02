0af78b7e49485d737a039fa3e6cf2ab4ab8885f0
Darth Maul <ngoodinquisitor3@gmail.com>
2024-02-02 16:15:29 +0500

    Correction

--- src/HomeWork20.java
+++ src/HomeWork20.java
@@ -1,6 +1,4 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class HomeWork20 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
