94246f1ab6e89e228e6dce838fd056da7bafb532
Darth Maul <ngoodinquisitor3@gmail.com>
2024-02-20 15:59:00 +0500

    New file

---
+++ src/HomeWorks/HomeWork25.java
@@ -0,0 +1,49 @@
+package HomeWorks;
+
+public class HomeWork25 {
+    public static void main(String[] args) {
+        Figure[] array = new Figure[3];
+        array[0] = new Figure(10,20);
+        array[1] = new Rectangle(10,20);
+        array[2] = new Triangle(10,20);
+        array[0].Area();
+        array[1].Area();
+        array[2].Area();
+    }
+}
+class Figure {
+    
+    int side1;
+    int side2;
+
+    public Figure(int side1, int side2) {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    void Area (){
+        System.out.println("Площадь фигуры не определена: " + (double) 0);
+    }
+}
+
+class Rectangle extends Figure{
+
+    public Rectangle(int side1, int side2) {
+        super(side1, side2);
+    }
+
+    @Override
+    void Area() {
+        System.out.println("Площадь прямоугольника: " + (double) side1 * side2);
+    }
+}
+class Triangle extends Figure{
+    public Triangle(int side1, int side2) {
+        super(side1, side2);
+    }
+
+    @Override
+    void Area() {
+        System.out.println("Площадь треугольника: " + (double) side1 * side2 / 2);
+    }
+}
