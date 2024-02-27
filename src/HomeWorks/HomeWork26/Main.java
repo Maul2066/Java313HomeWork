dbdcfe511c61647467c43f8cf0386caaf0509683
Darth Maul <ngoodinquisitor3@gmail.com>
2024-02-27 17:14:51 +0500

    fix file

--- src/HomeWorks/HomeWork26/Main.java
+++ src/HomeWorks/HomeWork26/Main.java
@@ -1,6 +1,6 @@
 package HomeWorks.HomeWork26;
 public class Main {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         Figure[] array = new Figure[3];
         array[0] = new Ball(4.5);
         array[1] = new Cylinder(2, 2);
