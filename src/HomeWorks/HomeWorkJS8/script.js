d8142fbea0ea44b166efea626e1c2301b88bc763
Darth Maul <ngoodinquisitor3@gmail.com>
2024-08-04 15:25:58 +0500

    HomeWorkJS8

---
+++ src/HomeWorks/HomeWorkJS8/script.js
@@ -0,0 +1,17 @@
+document.querySelector("#load").addEventListener("click", load);
+function load(){
+    let url = "https://jsonplaceholder.typicode.com/todos"
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let ul = document.querySelector('#list');
+            let html = data.map(function(item){
+                if(item.completed == true){
+                    return "<li> Пользователь " + item.userId + " выполнил задачу № " + item.id + " (" + item.title + ")</li>";
+                }
+            })
+            ul.insertAdjacentHTML("afterbegin", html.join(" "));
+        })
+}
